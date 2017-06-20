f9efb12fdfca35f2cbbff7c4effeff1e40484fef
Parvesh garg <parveshgarg24@gmail.com>
2017-06-20 18:12:36 +0530

    added project section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,7 +12,7 @@ This is empty on purpose! Your code to build the resume will go here.
  	},
  	"welcomeMessage" : "lorem ipsum dolor sit amet etc etc",
  	"skills" : ["awesomeness","algorithemic mind","delivering thing","adeptive"],
- 	"bioPic" : "images/fry.jpg"
+ 	"bioPic" : "images/myphoto.jpg"
  }
 
  var education = {
@@ -61,8 +61,14 @@ This is empty on purpose! Your code to build the resume will go here.
      {
      	"title": "tribute Page",
      	"dates": 2016,
-     	"decription" : "It's my first project made by me using front end developping coding . These project is made with only html and css. These tribute  Page is of Narendra Damodardas Modi",
-        "images" : "images/tribute-page.jpg"
+     	"description" : "It's my first project made by me using front end developping coding . These project is made with only html and css. These tribute  Page is of Narendra Damodardas Modi",
+        "images" : "images/tributepage.png"
+     },
+     {
+     	"title" : "Digital Calculator",
+     	"dates" : 2017,
+     	"description" : "It is digital calculator. All keys and calculation are working properly. Its my best project in javascript.",
+     	"images" : "images/calculator.png"
      }
  	]
  }
@@ -71,7 +77,26 @@ var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
+    	var contact = HTMLmobile.replace("%contact%","mobile");
+    	contact = contact.replace("%data%",bio.contacts.mobile);
+    	$("#topContacts").append(contact);
+    	contact = HTMLemail.replace("%contact%","email");
+    	contact = contact.replace("%data%",bio.contacts.email);
+    	$("#topContacts").append(contact);
+    	contact = HTMLgithub.replace("%contact%","github");
+    	contact = contact.replace("%data%",bio.contacts.github);
+    	$("#topContacts").append(contact);
+    	contact = HTMLlocation.replace("%contact%","location");
+    	contact = contact.replace("%data%",bio.contacts.location);
+    	$("#topContacts").append(contact);
+     var pic = HTMLbioPic.replace("%data%",bio.bioPic);
+    $("#header").append(pic);
+     var msg = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+     $("#header").append(msg);
+
  if(bio.skills.length>0) {
+
+
  	$("#header").append(HTMLskillsStart);
 
  	var formattedSkill = HTMLskills.replace("%data%",bio.skills[0]);
@@ -83,9 +108,47 @@ $("#header").prepend(formattedName);
  	formattedSkill = HTMLskills.replace("%data%",bio.skills[3]);
  	$("#skills").append(formattedSkill);
  }
+
+function displayWork(){
   for (key in work.jobs){
   	$("#workExperience").append(HTMLworkStart);
   	var jobEmploy = HTMLworkEmployer.replace("%data%",work.jobs[key].employer);
     var jobTitle = HTMLworkTitle.replace("%data%",work.jobs[key].title);
     $(".work-entry:last").append(jobEmploy+jobTitle);
+    var val = HTMLworkDates.replace("%data%",work.jobs[key].dates);
+    $(".work-entry:last").append(val);
+    val = HTMLworkDescription.replace("%data",work.jobs[key].description);
+    $(".work-entry:last").append(val);
+    val = HTMLworkLocation.replace("%data%",work.jobs[key].location);
+    $(".work-entry:last").append(val);
   }
+}
+
+displayWork();
+$(document).click(function(loc){
+	var x = loc.pageX;
+	var y = loc.pageY;
+	logClicks(x,y);
+});
+$("#main").append(internationalizeButton);
+function inName(name){
+	var arr=name.split(" ");
+	arr[0]=arr[0].slice(0,1).toUpperCase()+arr[0].slice(1);
+	arr[1]=arr[1].toUpperCase();
+	return arr.join(" ");
+}
+
+projects.display = function(){
+	for(key in projects.projects){
+		$("#projects").append(HTMLprojectStart);
+	    var project_data = HTMLprojectTitle.replace("%data%",projects.projects[key]["title"]);
+	    $(".project-entry:last").append(project_data);
+	    project_data = HTMLprojectDates.replace("%data%",projects.projects[key]["dates"]);
+	    $(".project-entry:last").append(project_data);
+	    project_data = HTMLprojectDescription.replace("%data%",projects.projects[key]["description"]);
+	    $(".project-entry:last").append(project_data);
+	    project_data = HTMLprojectImage.replace("%data%",projects.projects[key]["images"]);
+	    $(".project-entry:last").append(project_data);
+	  }
+}
+projects.display();
